bf31ab2563a793972c8467cacc713121ae888e87
Andy Hails <andy.hails@sagepub.co.uk>
2020-09-11 10:12:23 +0100

    Added docs

--- scripts/compile.sh
+++ scripts/compile.sh
@@ -1,2 +1,15 @@
+###
+# Use this script to compile and install locallly
+#
+# 1. Run this script from project root:
+# ```
+#   $ scripts/compile.sh
+# ```
+# 2. use pip to install
+# ```
+#   $ pip install dist/{{the package you just compiled}}
+# ```
+##
+
 rm -rf build dist *.egg-info
 python3 setup.py bdist_wheel
--- scripts/release.sh
+++ scripts/release.sh
@@ -1,2 +1,10 @@
+###
+# Use this script to release to pypi.
+# 1. Update version in setup.up
+# 2. Run this script from project root:
+# ```
+#   $ scripts/release.sh
+# ```
+##
 git tag $(python setup.py --version)
 git push --tags
