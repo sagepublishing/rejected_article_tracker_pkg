ffab6d39c9c637f422c044b66fd9193a0e19ca10
Adam Day <AdamDay1284@gmail.com>
2021-02-08 17:26:06 +0000

    clarify match article definition

--- rejected_article_tracker/src/SearchResult.py
+++ rejected_article_tracker/src/SearchResult.py
@@ -1,57 +1,89 @@
 from fuzzywuzzy import fuzz
 import numpy as np
+from gensim.parsing.preprocessing import preprocess_string, strip_tags
+from gensim.utils import deaccent
+
 
 
 class SearchResult:
-    def __init__(self, details, match_article: dict, clf, rank: int):
+    def __init__(self, match_article: dict, query_article: dict,  clf, rank: int):
         """
-        :param details: Article details from search provider (e.g. CrossRef)
-        :param match_article: The input article (e.g. from the user)
+        :param match_article: Article details from search provider (e.g. CrossRef)
+        :param query_article: The input article (e.g. from the user)
         :param clf: The classifier to score how similar the articles are.
         :param rank:
         """
-        self.details = details
+        self.query_article = query_article
         self.match_article = match_article
         self.clf = clf
         self.rank = rank
 
     def to_dict(self) -> dict:
-        details = self.details
-        details['authors_list'] = self.authors_list(details['author'])
-        details.update(self.match_names(match_authors=self.match_article['authors'], authors=details['authors_list']))
-        details['similarity'] = fuzz.ratio(self.match_article['manuscript_title'],
-                                           details['title']) if 'title' in details else 0
-        details['classifier_score'] = self.classify(details)
-        details['rank'] = self.rank
-        return details
+        match_article = self.match_article
+        match_article['authors_list'] = self.authors_list(match_article['author'])
+        match_article.update(self.match_names(
+                                        query_authors=self.query_article['authors'], 
+                                        match_authors=self.match_article['authors_list']
+                                        )
+                            )
+        match_article['similarity'] = fuzz.ratio(self.query_article['manuscript_title'],
+                                           match_article['title']) if 'title' in match_article else 0
+        match_article['classifier_score'] = self.classify(match_article)
+        match_article['rank'] = self.rank
+        return match_article
 
     @staticmethod
     def authors_list(authors: list) -> list:
         match_authors = []
         for author in authors:
-            given = author['given'] if 'given' in author else ''
-            family = author['family'] if 'family' in author else ''
+            given = author.get('given', '')
+            family = author.get('family', '')
             match_authors.append(given + '+' + family)
         return match_authors
 
-    @staticmethod
-    def match_names(match_authors, authors):
-        names1 = [(name[0], name[name.rfind('+') + 1:]) for name in match_authors.split(', ')]
-        names2 = [(name[0], name[name.rfind('+') + 1:]) for name in authors]
+    def pre_process_name(self, name):
+        """
+        Takes a string as input, removes accents and 
+        converts to lowercase
+        """
+        name = deaccent(name)
+        name = name.lower()
+        first_name = name[0]
+        last_name = name[name.rfind('+') + 1:]
+        first_name = first_name.replace('-','').replace('\'','').replace(' ','')
+        first_init = first_name[0]
+        last_name = last_name.replace('-','').replace('\'','').replace(' ','')
+        name = (first_init, last_name)
+        return name
+
+    # @staticmethod
+    def match_names(self, match_authors, query_authors):
+        names1 = list()
+        for name in query_authors.split(', '):
+            name = self.pre_process_name(name)
+            names1.append(name)
+        
+        names2 = list()
+        for name in match_authors:
+            name = self.pre_process_name(name)
+            names2.append(name)
+        
+        names2 = set(names2)
+
         return {
-            'author_match_one': any(name in names2 for name in names1),
-            'author_match_all': all(name in names2 for name in names1),
+            'author_match_one': int(any(name in names2 for name in names1)),
+            'author_match_all': int(all(name in names2 for name in names1)),
         }
 
-    def classify(self, details: dict):
-        items = [
-            details['similarity'],
-            details['author_match_all'],
-            details['score'],
+    def classify(self, match_article: dict):
+        predictors = [
+            match_article['similarity'],
+            match_article['author_match_all'],
+            match_article['score'],
             self.rank,
-            len(self.match_article['authors'].split(', '))
+            len(self.match_article['authors_list'])
         ]
-        X = np.array([float(x) for x in items])
+        X = np.array([float(x) for x in predictors])
         clf_scores = self.clf.predict_proba(np.reshape(X, (1, 5)))
         score = clf_scores[0][1]
         return score
