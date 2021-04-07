6c1c39486ce5639ac72a10f9605b116b22f445fe
Adam Day <AdamDay1284@gmail.com>
2021-04-07 15:20:30 +0100

    ensure models are where they are supposed to be

--- rejected_article_tracker/__init__.py
+++ rejected_article_tracker/__init__.py
@@ -7,3 +7,16 @@ from .src.ScholarOneRejectedArticlesMatch import ScholarOneRejectedArticlesMatch
 
 sys.path.append(os.path.abspath("."))
 
+# check models are where they are supposed to be
+from .src.ML.ArXivOAIPMH import ArXivOAIPMH
+ArXivOAIPMH().ensure_dirs()
+from .src.ML.config import Config as config
+import shutil
+lr_model_path = os.path.join(os.path.dirname(__file__),
+                            'src','ML','small_models','lr_model')
+dest_model_path = config.old_logreg_model_loc
+
+# and then move the models there.
+if not os.path.exists(dest_model_path):
+    os.mkdir(dest_model_path)
+    shutil.move(lr_model_path,dest_model_path)
