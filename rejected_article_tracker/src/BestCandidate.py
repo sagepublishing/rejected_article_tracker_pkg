59b8c591463a79f6a3e6f7fc353bcb42769edf3c
ad48 <adamday1284@gmail.com>
2022-04-12 18:13:48 +0100

    switch threshold to classifier_score

--- rejected_article_tracker/src/BestCandidate.py
+++ rejected_article_tracker/src/BestCandidate.py
@@ -7,9 +7,25 @@ class BestCandidate():
         self.threshold = threshold
 
     def find(self) -> dict:
-        candidates = list(filter(self.__filter, self.candidates))
-        result_scores = list(map(lambda c: c['classifier_score'], candidates))
-        return candidates[np.argmax(result_scores)] if len(candidates) > 0 else None
+        return list(filter(self.__filter, self.candidates))
 
     def __filter(self, candidate: dict):
-        return (candidate['similarity'] >= self.threshold) & candidate['author_match_one']
+
+        filters = [
+
+                ## In case you want to set a limit on fuzz.ratio
+                # (candidate['similarity'] >= 70) , 
+
+                candidate['classifier_score'] >=self.threshold,
+
+                ## basically ALL correct results match one at least. 
+                ## You don't really need this because the ML does this for you
+                #  bool(candidate['author_match_one']) ,
+
+                 ## this filter might be useful in tracking medrxiv etc
+                 ## so we can set the DOI of the preprint to the manuscript_id
+                 ## then this will filter it out.
+                #  str(candidate['DOI'])!=str(candidate['manuscript_id']),
+        ]
+
+        return all(filters)
